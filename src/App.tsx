e234cdcfd3b0edfb9596a02de69652f11994f630
Aigars Peda <aigarspeda@gmail.com>
2021-01-19 08:58:44 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import Img from "./images/img1.jpg";
+import { Provider } from "react-redux";
+import { BrowserRouter, matchPath } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import NavBar from "./components/NavBar";
+import store, { persistor } from "./redux/store";
+import AppRoutes from "./routes/AppRoutes";
 import "./styles.scss";
 
 const App: React.FC = () => {
+  const isMatch = matchPath(location.pathname, {
+    path: "/login" || "/signup",
+    exact: true,
+    strict: true
+  });
+
   return (
-    <div>
-      <p>Hello World</p>
-      <img src={Img} />
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <div className="App">
+            {!isMatch && <NavBar />}
+            <AppRoutes />
+          </div>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   );
 };
 
