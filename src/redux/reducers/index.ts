715fe778e2ea237932f319841f078893c7ce44d9
Aigars Peda <aigarspeda@gmail.com>
2021-01-21 10:31:11 +0200

    Update index.ts

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,21 +2,28 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import authReducer, { IAuthState } from "./authReducer";
+import errorReducer, { IErrorInitialState } from "./errorReducer";
 import userReducer, { IUserState } from "./userReducer";
 
 interface IDefaultState {
   user: IUserState;
   auth: IAuthState;
+  error: IErrorInitialState;
 }
 
 const persistConfig = {
   key: "root",
-  storage
+  storage,
+  // blacklist is what reducers not to persist
+  // in this case error after page reload
+  // will be gone
+  blacklist: ["error"]
 };
 
 const appReducers = combineReducers({
   auth: authReducer,
-  user: userReducer
+  user: userReducer,
+  error: errorReducer
 });
 
 const rootReducers = (state: IDefaultState | undefined, action: any) => {
