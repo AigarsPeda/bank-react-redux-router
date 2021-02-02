6c32ebbf22c19da779815708321fc8a32933524a
Aigars Peda <aigarspeda@gmail.com>
2021-02-02 05:32:41 +0200

    Update types.ts

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,40 +1,12 @@
-import { IUser } from "../types";
+import { AuthenticateActionTypes } from "./types/auth.types";
+import { CardsActionTypes } from "./types/cards.types";
+import { SetErrorActionTypes } from "./types/error.types";
+import { SetTransactionsActionTypes } from "./types/transactions.types";
+import { UserActionTypes } from "./types/user.types";
 
-// AUTH TYPES
-export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
-interface IAuthenticateUserAction {
-  type: typeof AUTHENTICATE_USER;
-  payload: string;
-}
-
-export const UNAUTHENTICATED_USER = "UNAUTHENTICATED_USER";
-interface IUnauthenticatedUserAction {
-  type: typeof UNAUTHENTICATED_USER;
-}
-
-export type AuthenticateActionTypes =
-  | IAuthenticateUserAction
-  | IUnauthenticatedUserAction;
-
-// USER TYPES
-export const SET_USER_DATA = "SET_USER_DATA";
-interface ISetUserAction {
-  type: typeof SET_USER_DATA;
-  payload: IUser;
-}
-
-export const CLEAR_USER_DATA = "CLEAR_USER_DATA";
-interface IClearUserDataAction {
-  type: typeof CLEAR_USER_DATA;
-  // payload: ScreamType[];
-}
-
-export type UserActionTypes = ISetUserAction | IClearUserDataAction;
-
-// ERROR TYPES
-export const SET_ERROR = "SET_ERROR";
-interface ISetErrorAction {
-  type: typeof SET_ERROR;
-  payload: string;
-}
-export type SetErrorActionTypes = ISetErrorAction;
+export type AppActionsTypes =
+  | UserActionTypes
+  | SetTransactionsActionTypes
+  | SetErrorActionTypes
+  | CardsActionTypes
+  | AuthenticateActionTypes;
