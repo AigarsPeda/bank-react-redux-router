8a07cf0089545e30ab12b22ea612ff6d1b993eb2
Aigars Peda <aigarspeda@gmail.com>
2021-02-04 07:45:08 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,7 @@ export type IUser = {
   email: string;
   created_on: string;
   client_id: number;
+  clients_total_balance: string;
 };
 
 export type IToken = string;
@@ -24,4 +25,24 @@ export type IUserSignUp = {
 export type IUserLogIn = {
   email: string;
   password: string;
+};
+
+export type ICards = {
+  card_id: number;
+  card_no: string;
+  bank_name: string;
+  total_balance: string;
+  client_id: number;
+  lender: boolean;
+};
+
+export type ITransactions = {
+  formatted_date: string;
+  withdraw_amount: string | null;
+  deposit_amount: string | null;
+  balance: string;
+  transaction_id: string;
+  card_id: string;
+  deposit_description: string | null;
+  withdraw_description: string | null;
 };
