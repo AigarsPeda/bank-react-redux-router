7560572284686d8013abd1a3297a0704f3aa0ac4
Aigars Peda <aigarspeda@gmail.com>
2021-02-06 09:20:23 +0200

    Update DashboardPage.tsx

--- src/pages/dashboardPage/DashboardPage.tsx
+++ src/pages/dashboardPage/DashboardPage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-
 import AccountOverview from "../../components/AccountOverview/AccountOverview";
 import ClientActions from "../../components/ClientActions/ClientActions";
 import NavBar from "../../components/NavBar/NavBar";
