f0aa8875bd10f651b48834b4a68aa73543321a6a
Aigars Peda <aigarspeda@gmail.com>
2021-02-12 12:55:58 +0200

    Create transactions.ts

---
+++ src/redux/actions/transactions.ts
@@ -0,0 +1,66 @@
+import { ThunkAction } from "redux-thunk";
+import { callAPI } from "../../services/callAPI";
+import { ITransactions } from "../../types";
+import { RootStateType } from "../reducers";
+import {
+  SetTransactionsActionTypes,
+  SET_TRANSACTION,
+  SET_TRANSACTION_IS_LOADING
+} from "../types/transactions.types";
+
+type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootStateType,
+  unknown,
+  SetTransactionsActionTypes
+>;
+
+// get cards transactions
+export const getTransactions = (id: string): AppThunk => async (dispatch) => {
+  try {
+    dispatch({
+      type: SET_TRANSACTION_IS_LOADING,
+      payload: true
+    });
+    // in callAPI if there are token in REDUX STORE
+    // it is added to AUTHORIZATION headers
+    const response: ITransactions[] = await callAPI({
+      url: `/transactions/${id}`,
+      method: "GET"
+    });
+
+    // console.log("RESPONSE: ", response);
+
+    dispatch({
+      type: SET_TRANSACTION,
+      payload: response
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get all transactions
+export const getAllTransactions = (): AppThunk => async (dispatch) => {
+  try {
+    dispatch({
+      type: SET_TRANSACTION_IS_LOADING,
+      payload: true
+    });
+    // in callAPI if there are token in REDUX STORE
+    // it is added to AUTHORIZATION headers
+    const response: ITransactions[] = await callAPI({
+      url: `/transactions/all_transactions`,
+      method: "GET"
+    });
+
+    // console.log("RESPONSE xxxxxx: ", response);
+
+    dispatch({
+      type: SET_TRANSACTION,
+      payload: response
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
