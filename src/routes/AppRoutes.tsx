9da62b979d6e3724f344aca31ee39134c343eaa2
Aigars Peda <aigarspeda@gmail.com>
2021-02-18 06:02:16 +0200

    Update AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,8 +3,10 @@ import { Route, Switch } from "react-router-dom";
 
 // pages
 import DashboardPage from "../pages/dashboardPage/DashboardPage";
+import DepositPage from "../pages/depositPage/DepositPage";
 import LoginPage from "../pages/loginPage/LoginPage";
 import NoMatchPage from "../pages/NoMatch/NoMatchPage";
+import ProfilePage from "../pages/profilePage/ProfilePage";
 import SignUpPage from "../pages/signUpPage/SignUpPage";
 import AuthRoute from "../utils/AuthRoute";
 
@@ -14,6 +16,9 @@ const AppRoutes: React.FC = () => {
       <Route exact path="/signup" component={SignUpPage} />
       <Route exact path="/login" component={LoginPage} />
       <AuthRoute exact path="/" component={DashboardPage} />
+      <AuthRoute exact path="/profile" component={ProfilePage} />
+      <AuthRoute exact path="/deposit" component={DepositPage} />
+
       <Route component={NoMatchPage} />
     </Switch>
   );
