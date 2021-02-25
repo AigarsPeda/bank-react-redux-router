5fe0fa2058021df96623b8cfffd52052518e3f40
Aigars Peda <aigarspeda@gmail.com>
2021-02-25 09:19:04 +0200

    Update cardsReducer.ts

--- src/redux/reducers/cardsReducer.ts
+++ src/redux/reducers/cardsReducer.ts
@@ -2,16 +2,19 @@ import { ICards } from "../../types";
 import {
   CardsActionTypes,
   CLEAR_CARDS_DATA,
+  LOADING_CARDS_DATA,
   SET_CARDS_DATA
 } from "../types/cards.types";
 
 export interface ICardsState {
   cards: ICards[];
+  loadingCardsData: boolean;
 }
 
 //  State
 const initialState: ICardsState = {
-  cards: []
+  cards: [],
+  loadingCardsData: false
 };
 
 export default (
@@ -19,10 +22,17 @@ export default (
   action: CardsActionTypes
 ): ICardsState => {
   switch (action.type) {
+    case LOADING_CARDS_DATA:
+      return {
+        ...state,
+        loadingCardsData: true
+      };
+
     case SET_CARDS_DATA:
       return {
         ...state,
-        cards: action.payload
+        cards: action.payload,
+        loadingCardsData: false
       };
 
     case CLEAR_CARDS_DATA:
