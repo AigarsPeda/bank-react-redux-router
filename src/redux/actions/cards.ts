ab85da11c0abccc0969f22e2530c33306b98eec2
Aigars Peda <aigarspeda@gmail.com>
2021-02-25 09:18:58 +0200

    Update cards.ts

--- src/redux/actions/cards.ts
+++ src/redux/actions/cards.ts
@@ -2,7 +2,11 @@ import { ThunkAction } from "redux-thunk";
 import { callAPI } from "../../services/callAPI";
 import { ICards } from "../../types";
 import { RootStateType } from "../reducers";
-import { CardsActionTypes, SET_CARDS_DATA } from "../types/cards.types";
+import {
+  CardsActionTypes,
+  LOADING_CARDS_DATA,
+  SET_CARDS_DATA
+} from "../types/cards.types";
 
 type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
@@ -29,5 +33,40 @@ export const getCards = (): AppThunk => async (dispatch) => {
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+interface IDeposit {
+  deposit_amount: number;
+  deposit_description: string;
+}
+
+export const makeDeposit = (cardId: string, data: IDeposit): AppThunk => async (
+  dispatch
+) => {
+  dispatch({
+    type: LOADING_CARDS_DATA,
+    payload: true
+  });
+
+  try {
+    const response: string = await callAPI({
+      url: `/deposit/${cardId}`,
+      method: "POST",
+      data: data
+    });
+
+    dispatch({
+      type: LOADING_CARDS_DATA,
+      payload: false
+    });
+
+    return response;
+
+    // console.log("response: ", response);
+  } catch (error) {
+    console.log(error);
+
+    return "Something went wrong!";
   }
 };
